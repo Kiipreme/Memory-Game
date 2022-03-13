51114dc73aaf6049875b62809980110cf04faa74
Kelvin <kelvin.a.garcia21@gmail.com>
2022-03-12 21:46:11 -0500

    added so far

---
+++ app.js
@@ -0,0 +1,28 @@
+const startButton = document.querySelector(".start");
+const restartButton = document.querySelector(".restart");
+const cards = document.querySelector(".cards");
+const hide = document.querySelector(".hidden");
+
+
+// Array of cards values.
+const pairValues = [
+  "A",
+  "A",
+  "B",
+  "B",
+  "C",
+  "C",
+  "D",
+  "D",
+  "E",
+  "E",
+  "F",
+  "F",
+  "G",
+  "G",
+  "H",
+  "H",
+  "I",
+  "I",
+]; 
+
